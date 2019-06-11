fe15261e6fad02fc1fe1fb2f6882f88eecb156eb
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2019-06-11 19:41:26 +0200

    Create Input-output.cpp

---
+++ Basics Cpp/Input-output.cpp
@@ -0,0 +1,16 @@
+## Input-Output Problem:
+## link : https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/tutorial/
+
+
+#include <stdio.h>
+
+int main()
+{
+int x;
+char name[16];
+scanf("%d",&x);
+scanf("%s",&name);
+printf("%d\n",x*2);
+printf("%s",name);
+return 0;
+}
