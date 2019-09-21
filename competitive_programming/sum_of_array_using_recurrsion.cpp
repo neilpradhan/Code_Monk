b3de46180aba49bcceec30fedf1f2359be9e82de
Neil Pradhan <34929271+neilpradhan@users.noreply.github.com>
2019-09-21 08:41:19 +0200

    Update sum_of_array_using_recurrsion.cpp

--- competitive_programming/sum_of_array_using_recurrsion.cpp
+++ competitive_programming/sum_of_array_using_recurrsion.cpp
@@ -3,11 +3,7 @@
 using namespace std;
 
 int sum(int input[], int n) {
-  /* Don't write main().
-     Don't read input, it is passed as function argument.
-     Return output and don't print it.
-     Taking input and printing output is handled automatically.
-  */
+
     
     
     
